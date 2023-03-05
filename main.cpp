f137080a025ce161f97dbff30c47668740392fdb
TheRealMerlin <iankuffel@gmail.com>
2023-03-05 14:37:41 -0600

    multi-tracking functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <arrow/api.h>
+#include <arrow/io/api.h>
+#include <parquet/arrow/writer.h>
+#include <parquet/exception.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/tracking.hpp>
 #include <opencv2/core.hpp>
@@ -55,6 +60,10 @@ int parse_args(int argc, char** argv) {
 				std::cerr << "-d DISTANCE option requires one argument" << std::endl;
 				return 1;
 			}
+		} else if(strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--timeit") == 0) {
+			TIMEIT = true;
+		} else if(strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--show") == 0) {
+			SHOW = true;
 		} else if(PATH.compare("") == 0) {
 			std::ifstream test(argv[i]);
 			if(!test) {
@@ -95,11 +104,11 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 
+	// Calculates ratio (microns : px)
+	double ratio = DISTANCE / PX_DISTANCE;
 
-	cv::Ptr<cv::Tracker> tracker = cv::TrackerCSRT::create();
-
+	// Opens video and reads into frame
 	cv::VideoCapture video(PATH);
-
 	cv::Mat frame;
 	bool ok = video.read(frame);
 	if(!video.isOpened()) {
@@ -107,25 +116,95 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
+	// Gets number of frames in video
 	int num_frames = (int)video.get(cv::CAP_PROP_FRAME_COUNT);
 
-	cv::Rect bbox = cv::selectROI(frame, false);
-	cv::imshow("Tracking", frame);
-	tracker->init(frame, bbox);
-
-	for(int j = 0; j < num_frames; j++) {
-		bool ok = tracker->update(frame, bbox);
-		if(ok) {
-			cv::rectangle(frame, bbox, cv::Scalar(255, 0, 0), 2, 1);
-		}else {
-			std::cerr << "Tracking Failure Detected!" << std::endl;
-		}
-
-		cv::imshow("Tracking", frame);
-
-		int k = cv::waitKey(1);
-		if(k == 27) { break; }
+	// Creates a vector of trackers and bboxes
+	std::vector<cv::Ptr<cv::Tracker>> trackers;
+	std::vector<cv::Rect> bboxes;
+	for(int i = 0; i < NUM_DROPLETS; i++) {
+		trackers.push_back(cv::TrackerCSRT::create());
+		bboxes.push_back(cv::selectROI(frame, false));
 	}
+
+	// Close select ROI window
+	cv::destroyAllWindows();
+
+	// Initialize trackers w/ respective bboxes
+	for(int i = 0; i < NUM_DROPLETS; i++) {
+		trackers[i]->init(frame, bboxes[i]);
+	}
+
+	// Initialize 2-D array of x and y values
+	std::vector<std::vector<double>> x(NUM_DROPLETS, std::vector<double>(num_frames, 0.0));
+	std::vector<std::vector<double>> y(NUM_DROPLETS, std::vector<double>(num_frames, 0.0));
+
+	// Store first frame values
+	for(int i = 0; i < NUM_DROPLETS; i++) {
+		x[i][0] = (bboxes[i].x + bboxes[i].width / 2) * ratio;
+		y[i][0] = (bboxes[i].y + bboxes[i].height / 2) * ratio;
+	}
+
+	// Tracking loop
+	for(int j = 1; j < num_frames; j++) {
+		// Read next frame
+		video.read(frame);
+		
+		// Update each tracker
+		for(int i = 0; i < NUM_DROPLETS; i++) {
+			ok = trackers[i]->update(frame, bboxes[i]);
+			if(ok) {
+				// Tracking success
+				x[i][j] = (bboxes[i].x + bboxes[i].width / 2) * ratio;
+				y[i][j] = (bboxes[i].y + bboxes[i].height / 2) * ratio;
+
+				// Draw rectangle on frame if displaying trackers
+				if(SHOW) {
+					cv::rectangle(frame, bboxes[i], cv::Scalar(255, 0, 0), 2, 1);
+				}
+			} else {
+				// Tracking failure
+				std::cerr << "Tracking Failure Detected!\tDroplet: " << i + 1 << "\tFrame: " << j << std::endl;
+			}
+		}
+		
+		// Update tracking display window
+		if(SHOW) {
+			cv::imshow("Tracking", frame);
+			int k = cv::waitKey(1);
+			if(k == 27) { break; }
+		}
+	}
+
+	// Create schema for output file
+	arrow::FieldVector fields;
+	for(int i = 0; i < NUM_DROPLETS; i++) {
+		fields.push_back(arrow::field("x_" + std::to_string(i), arrow::float64()));
+		fields.push_back(arrow::field("y_" + std::to_string(i), arrow::float64()));
+	}
+	std::shared_ptr<arrow::Schema> schema = arrow::schema(fields);
+
+	// Store data into arrow::Table for output
+	arrow::ArrayVector array_vector;
+	for(int i = 0; i < NUM_DROPLETS; i++) {
+		arrow::DoubleBuilder dbuilder;
+		dbuilder.AppendValues(x[i]);
+		std::shared_ptr<arrow::DoubleArray> data_array;
+		dbuilder.Finish(&data_array);
+		array_vector.push_back(data_array);
+	}
+	std::cerr << array_vector.at(0) << std::endl;
+	std::shared_ptr<arrow::Table> table = arrow::Table::Make(schema, array_vector);
+
+	// Write table to output file
+	std::shared_ptr<arrow::io::FileOutputStream> outfile;
+	PARQUET_THROW_NOT_OK(arrow::io::FileOutputStream::Open("test.parquet", &outfile));
+	PARQUET_THROW_NOT_OK(parquet::arrow::WriteTable(*table.get(), arrow::default_memory_pool(), outfile, 3));
+
+	// Garbage collection
+	outfile->Close();
+	video.release();
+	cv::destroyAllWindows();
 
 	return 0;
 }
